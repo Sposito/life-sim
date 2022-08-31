3674a9e8ed1a1abf2b7cdde529574f5e7df5cfe4
Thiago Sposito <sposito.thiago@gmail.com>
2022-08-31 13:18:37 -0300

    Particle structure

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,53 @@
 extern crate sdl2;
 
 
+use std::sync::Arc;
+use std::string::String;
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use std::time::Duration;
 use rand::Rng;
 
+// There will be 16 particles:
+// coding: a, t, g
+// encoded: aine, bine, cine, dine, eine, fine, gine, hine, gine
+// energy: sugar, oil
+
+pub struct Particle{
+    id : i16,
+    kind: i16,
+    position: (i16, i16),
+    velocity: (i16, i16),
+    color: Color,
+    charge: i8,
+    chain0: Option<Arc<Particle>>,
+    chain1: Option<Arc<Particle>>,
+    chain2: Option<Arc<Particle>>,
+}
+
+pub fn get_a(pos:(i16, i16)) -> Particle {
+    let a: Particle = Particle {
+        id: 0,
+        kind: 0 << 0,
+        position: pos,
+        velocity: (0, 0),
+        color: Color::RGB(255, 255, 0),
+        charge: 2,
+        chain0 : None,
+        chain1: None,
+        chain2: None,
+    };
+    a
+
+
+}
+
+
 pub fn main() {
+    let m:Particle = get_a((0,0));
+    print!("{:?}", m.position);
+
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
